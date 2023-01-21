5868869b40ab9111498105e9738b62abc63aa2e0
rupsha014 <rupshasarkar14@gmail.com>
2023-01-21 00:25:48 -0800

    change2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Header from './header';
 import Meme from './meme';
-// import Image from './image';
-
-
+import { Helmet } from 'react-helmet'
+import logo from './daatkelano image.jpeg'
 
 
 function App() {
   return (
     <div>
+      <Helmet>
+          <title>Meme generator</title>
+          <link rel="icon"  href={logo} sizes="16x16" />
+        </Helmet>
       <Header />
       <Meme />
+      
     </div>
   );
 }
